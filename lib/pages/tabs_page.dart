1185fb573a7770dd010e2452c5c583d8f1aa2360
Giacomo Ferlaino <giacomo.ferlaino@outlook.com>
2020-06-28 18:18:13 +0200

    feat: Added navigation drawer

--- lib/pages/tabs_page.dart
+++ lib/pages/tabs_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:meals_app/pages/favorites_page.dart';
 
 import '../pages/categories_page.dart';
+import '../pages/favorites_page.dart';
+import '../widgets/main_drawer.dart';
 
 class TabsPage extends StatefulWidget {
   @override
@@ -32,6 +33,9 @@ class _TabsPageState extends State<TabsPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text(_pages[_selectedPageIndex]['title']),
+      ),
+      drawer: Drawer(
+        child: MainDrawer(),
       ),
       body: _pages[_selectedPageIndex]['page'],
       bottomNavigationBar: BottomNavigationBar(
